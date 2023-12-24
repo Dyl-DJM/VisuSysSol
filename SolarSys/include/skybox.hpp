5d76e81149d934b60e4d953dd9c0841fc827a581
Dylan DE JESUS <dejesusmilitar@edu.univ-eiffel.fr>
2023-12-24 12:26:44 +0100

    Cleared the Skybox module
    The documentation of the skybox is now available

--- SolarSys/include/skybox.hpp
+++ SolarSys/include/skybox.hpp
@@ -1,4 +1,14 @@
-
+/*
+======================================================
+=  													 =
+=     Made by Kevin QUACH and Dylan DE JESUS	     =
+=													 =
+=													 =
+=  Skybox definition, it represents a textured cube  =
+=  this object can be used in an index buffer.       =
+=  													 =
+======================================================
+*/
 
 #pragma once
 
@@ -13,139 +23,103 @@
 
 using namespace glimac;
 
+/**
+ * @brief Textured cube representation.
+ ********************************************************************************/
 class Skybox
 {
 public:
-    Skybox(FilePath filePath, GLuint textID)
-    {
-        build(filePath, textID);
-    }
+    /**
+     * @brief Builds a textured cube.
+     *
+     * Makes a skybox representation using the index buffer system.
+     * This means that there are both a data storage and an index one.
+     *
+     * @param filePath A FilePath object (see the FilePath class in glimac).
+     * @param textID An ID that describes a texture that has been loaded.
+     ********************************************************************************/
+    Skybox(FilePath filePath, GLuint textID); // TODO : Would be better to give immediately a ShaderManager Object instead of its parameters
 
-    GLsizei nbVertices() const
-    {
-        return _cube.size();
-    }
+    /**
+     * @brief Retrieves the number of vertices.
+     *
+     * It gives the amount of vertices which describes the cube. (8 vertices minimum)
+     *
+     * @return The amount of vertices of the cube.
+     ********************************************************************************/
+    GLsizei nbVertices() const;
 
-    const ShapeVertex *data() const
-    {
-        return _cube.data();
-    }
+    /**
+     * @brief Retrieves the vertices objects of the cube.
+     *
+     * The vertices retrieved are ShapeVertex objects (see the common module
+     * in glimac).
+     * These objects contains information about the 3D coordinates for a vertex,
+     * its texture coordinates and its normals.
+     *
+     * @return A pointer on all the vertices of the cube.
+     ********************************************************************************/
+    const ShapeVertex *data() const;
 
-    const std::vector<GLuint> getTextIDs() const
-    {
-        return _texts;
-    }
+    /**
+     * @brief Retrieves all the texture IDs binded to the cube.
+     *
+     * @return A reference on a vector that contains all the textures binded to the
+     * cube.
+     ********************************************************************************/
+    const std::vector<GLuint> &getTextIDs() const;
 
-    std::shared_ptr<ShaderManager> getShaderManager()
-    {
-        return _shader;
-    }
+    /**
+     * @brief Retrieves the ShaderManager of the cube.
+     *
+     * The ShaderManager (see the ShaderManager module) allows the cube to send
+     * its information to the shaders.
+     *
+     * @return A pointer on the ShaderManager binded to this cube.
+     ********************************************************************************/
+    std::shared_ptr<ShaderManager> getShaderManager();
 
-    const Matrices &getMatrices() const
-    {
-        return _matrices;
-    }
+    /**
+     * @brief Retrieves the transformation matrices of the cube.
+     *
+     * Brings a Matrices object (see the Matrices module), that contains MV, MVP and
+     * normals matrices.
+     *
+     * @return A reference on the matrices.
+     ********************************************************************************/
+    const Matrices &getMatrices() const;
 
-    const uint32_t *getIndexes() const
-    {
-        return _indexes.data();
-    }
+    /**
+     * @brief Retrieves the indexes that describe the cube triangles.
+     *
+     * The indexes are refering to one of the points that describe the cube. In
+     * this case, the cube is drawn thanks to triangle shapes.
+     *
+     * The triangles defined by three points are given by three indexes.
+     *
+     * @return A pointer on the indexes.
+     ********************************************************************************/
+    const uint32_t *getIndexes() const;
 
-    uint nbIndexes() const
-    {
-        return _indexes.size();
-    }
+    /**
+     * @brief Retrieves the number of indexes stored.
+     *
+     * @return The amount of indexes used for this cube.
+     ********************************************************************************/
+    uint nbIndexes() const;
 
 private:
-    void
-    build(FilePath filePath, GLuint textID)
-    {
-        /*
-              5   *---------*    4
-                 /|        /|
-            0   *---------* |  3
-                | |       | |
-               6| |_______|_*  7
-                |/        |/
-             1  *---------*  2
+    /**
+     * @brief Builds the cube representing the skybox.
+     *
+     * Creates a cube in 3D dimension thanks to 8 points defined and indexes that
+     * describe how this shape can be drawn with triangles.
+     ********************************************************************************/
+    void build(FilePath filePath, GLuint textID);
 
-        */
-
-        GLfloat points[8][3] = {
-            // Foreground square
-            {-1.0, 1.0, 1.0},  // Point 1
-            {-1.0, -1.0, 1.0}, // Point 2
-            {1.0, -1.0, 1.0},  // Point 3
-            {1.0, 1.0, 1.0},   // Point 4
-
-            // Background square
-            {1.0, 1.0, -1.0},   // Point 5
-            {-1.0, 1.0, -1.0},  // Point 6
-            {-1.0, -1.0, -1.0}, // Point 7
-            {1.0, -1.0, -1.0}   // Point 8
-        };
-
-        int text2DCoords[4][2] = {
-            // Face text
-            {0, 0}, // Point 1
-            {0, 1}, // Point 2
-            {1, 0}, // Point 3
-            {1, 1}, // Point 4
-        };
-
-        for (int i = 0; i < 8; i++)
-        {
-            ShapeVertex vertex;
-
-            vertex.position = glm::vec3(points[i][0], points[i][1], points[i][2]);
-
-            vertex.texCoords.x = text2DCoords[i % 4][0];
-            vertex.texCoords.y = text2DCoords[i % 4][1];
-
-            vertex.normal = glm::normalize(vertex.position);
-
-            _cube.push_back(vertex);
-        }
-
-        _matrices.init(1000, 1000, 0, 0, 0.5);
-
-        _shader = std::make_shared<Shader1Texture>(filePath);
-
-        _texts.push_back(textID);
-
-        _indexes = {
-            // Front face
-            0, 1, 2, // Triangle 1
-            2, 0, 3, // Triangle 2
-
-            // Upper face
-            3, 4, 5, // Triangle 3
-            5, 0, 3, // Triangle 4
-
-            // Right face
-            3, 4, 7, // Triangle 5
-            7, 3, 2, // Triangle 6
-
-            // Lower face
-            2, 7, 6, // Triangle 7
-            6, 2, 1, // Triangle 8
-
-            // Left Face
-            1, 0, 5, // Triangle 9
-            5, 1, 6, // Triangle 10
-
-            // Back Face
-            6, 5, 4, // Triangle 9
-            4, 6, 7  // Triangle 10
-        };
-    }
-
-    std::vector<ShapeVertex>
-        _cube;
-    std::shared_ptr<ShaderManager> _shader;
-    Matrices _matrices;
-
-    std::vector<GLuint> _texts;
-
-    std::vector<uint32_t> _indexes;
+    std::vector<ShapeVertex> _cube;         // Vertices that describe the shape
+    std::shared_ptr<ShaderManager> _shader; // Shader binded to the cube
+    Matrices _matrices;                     // Transformation matrices of the cube
+    std::vector<GLuint> _texts;             // Textures Ids
+    std::vector<uint32_t> _indexes;         // Indexes representing the triangles that draw the cube in 3D
 };
--- SolarSys/src/skybox.cpp
+++ SolarSys/src/skybox.cpp
@@ -1,3 +1,200 @@
-
+/*
+======================================================
+=  													 =
+=     Made by Kevin QUACH and Dylan DE JESUS	     =
+=													 =
+=													 =
+=  Skybox definition, it represents a textured cube  =
+=  this object can be used in an index buffer.       =
+=  													 =
+======================================================
+*/
 
 #include "include/skybox.hpp"
+
+/**
+ * @brief Builds a textured cube.
+ *
+ * Makes a skybox representation using the index buffer system.
+ * This means that there are both a data storage and an index one.
+ *
+ * @param filePath A FilePath object (see the FilePath class in glimac).
+ * @param textID An ID that describes a texture that has been loaded.
+ ********************************************************************************/
+Skybox::Skybox(FilePath filePath, GLuint textID) // TODO : Would be better to give immediately a ShaderManager Object instead of its parameters
+{
+    build(filePath, textID);
+}
+
+/**
+ * @brief Retrieves the number of vertices.
+ *
+ * It gives the amount of vertices which describes the cube. (8 vertices minimum)
+ *
+ * @return The amount of vertices of the cube.
+ ********************************************************************************/
+GLsizei Skybox::nbVertices() const
+{
+    return _cube.size();
+}
+
+/**
+ * @brief Retrieves the vertices objects of the cube.
+ *
+ * The vertices retrieved are ShapeVertex objects (see the common module
+ * in glimac).
+ * These objects contains information about the 3D coordinates for a vertex,
+ * its texture coordinates and its normals.
+ *
+ * @return A pointer on all the vertices of the cube.
+ ********************************************************************************/
+const ShapeVertex *Skybox::data() const
+{
+    return _cube.data();
+}
+
+/**
+ * @brief Retrieves all the texture IDs binded to the cube.
+ *
+ * @return A reference on a vector that contains all the textures binded to the
+ * cube.
+ ********************************************************************************/
+const std::vector<GLuint> &Skybox::getTextIDs() const
+{
+    return _texts;
+}
+
+/**
+ * @brief Retrieves the ShaderManager of the cube.
+ *
+ * The ShaderManager (see the ShaderManager module) allows the cube to send
+ * its information to the shaders.
+ *
+ * @return A pointer on the ShaderManager binded to this cube.
+ ********************************************************************************/
+std::shared_ptr<ShaderManager> Skybox::getShaderManager()
+{
+    return _shader;
+}
+
+/**
+ * @brief Retrieves the transformation matrices of the cube.
+ *
+ * Brings a Matrices object (see the Matrices module), that contains MV, MVP and
+ * normals matrices.
+ *
+ * @return A reference on the matrices.
+ ********************************************************************************/
+const Matrices &Skybox::getMatrices() const
+{
+    return _matrices;
+}
+
+/**
+ * @brief Retrieves the indexes that describe the cube triangles.
+ *
+ * The indexes are refering to one of the points that describe the cube. In
+ * this case, the cube is drawn thanks to triangle shapes.
+ *
+ * The triangles defined by three points are given by three indexes.
+ *
+ * @return A pointer on the indexes.
+ ********************************************************************************/
+const uint32_t *Skybox::getIndexes() const
+{
+    return _indexes.data();
+}
+
+/**
+ * @brief Retrieves the number of indexes stored.
+ *
+ * @return The amount of indexes used for this cube.
+ ********************************************************************************/
+uint Skybox::nbIndexes() const
+{
+    return _indexes.size();
+}
+
+/**
+ * @brief Builds the cube representing the skybox.
+ *
+ * Creates a cube in 3D dimension thanks to 8 points defined and indexes that
+ * describe how this shape can be drawn with triangles.
+ ********************************************************************************/
+void Skybox::build(FilePath filePath, GLuint textID)
+{
+    /*
+          5   *---------*    4
+             /|        /|
+        0   *---------* |  3
+            | |       | |
+           6| |_______|_*  7
+            |/        |/
+         1  *---------*  2
+
+    */
+
+    GLfloat points[8][3] = {
+        // Foreground square
+        {-1.0, 1.0, 1.0},  // Point 1
+        {-1.0, -1.0, 1.0}, // Point 2
+        {1.0, -1.0, 1.0},  // Point 3
+        {1.0, 1.0, 1.0},   // Point 4
+
+        // Background square
+        {1.0, 1.0, -1.0},   // Point 5
+        {-1.0, 1.0, -1.0},  // Point 6
+        {-1.0, -1.0, -1.0}, // Point 7
+        {1.0, -1.0, -1.0}   // Point 8
+    };
+
+    int text2DCoords[4][2] = {
+        // Face text
+        {0, 0}, // Point 1
+        {0, 1}, // Point 2
+        {1, 0}, // Point 3
+        {1, 1}, // Point 4
+    };
+
+    for (int i = 0; i < 8; i++)
+    {
+        ShapeVertex vertex;
+
+        vertex.position = glm::vec3(points[i][0], points[i][1], points[i][2]);
+        vertex.texCoords.x = text2DCoords[i % 4][0];
+        vertex.texCoords.y = text2DCoords[i % 4][1];
+        vertex.normal = glm::normalize(vertex.position);
+
+        _cube.push_back(vertex);
+    }
+
+    // Initialize the attributes of the class
+    _matrices.init(1000, 1000, 0, 0, 0.5); // TODO : Actually there are manual arguments, need to pass the windows dimensions instead of 1000, 1000
+    _shader = std::make_shared<Shader1Texture>(filePath);
+    _texts.push_back(textID);
+    _indexes = {
+        // Front face
+        0, 1, 2, // Triangle 1
+        2, 0, 3, // Triangle 2
+
+        // Upper face
+        3, 4, 5, // Triangle 3
+        5, 0, 3, // Triangle 4
+
+        // Right face
+        3, 4, 7, // Triangle 5
+        7, 3, 2, // Triangle 6
+
+        // Lower face
+        2, 7, 6, // Triangle 7
+        6, 2, 1, // Triangle 8
+
+        // Left Face
+        1, 0, 5, // Triangle 9
+        5, 1, 6, // Triangle 10
+
+        // Back Face
+        6, 5, 4, // Triangle 9
+        4, 6, 7  // Triangle 10
+    };
+}
